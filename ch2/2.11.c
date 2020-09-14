9490b5d95e737f4e25ea79785333ffc05dafaac8
vincent178 <vh7157@gmail.com>
2020-09-15 07:00:29 +0800

    ch2.11

---
+++ ch2/2.11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void inplace_swap(int *x, int *y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+void reverse_arrary(int a[], int cnt) {
+  int first, last;
+  for (first = 0, last = cnt - 1; first < last; first++,last--)
+    inplace_swap(&a[first], &a[last]);
+}
+
+void print_array(int a[], int cnt) {
+  int loop;
+  for (loop = 0; loop < cnt; loop++)
+    printf("%d ", a[loop]);
+  printf("\n");
+}
+
+void main() {
+  int a[4] = {1, 2, 3, 4};
+  reverse_arrary(a, 4);
+  print_array(a, 4);
+
+  int b[5] = {1, 2, 3, 4, 5};
+  reverse_arrary(b, 5);
+  print_array(b, 5);
+}
+
